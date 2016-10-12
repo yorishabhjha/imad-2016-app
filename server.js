a27a1c2dacd59ba8148ffffc56022b355949ef14
yorishabhjha <yorishabh.jha@gmail.com>
2016-10-12 14:48:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('page1/', function (req, res) {
+  res.sendFile('page1 requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
