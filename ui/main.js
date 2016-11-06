219f6d140a2b729ea2300d8ba1fea98217c6fdb2
yorishabhjha <yorishabh.jha@gmail.com>
2016-11-06 14:47:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+	if(request.readyState === XMLHttpRequest.Done)
+	{
+		if(request.status === 200)
+		{
+			var count = request.responseText;
+			var c = document.getElementById('count');
+			c.innerHTML = count.toString();
+		}
+	}
+};
+
+request.open('GET','http://yorishabhjha.imad.hasura-app.io',true);
+request.send(null);
